b06ba070090c63ba95f3f089332b6d72e9a5cfa9
Dan Gla <74205610+DanGlado@users.noreply.github.com>
2023-11-06 22:24:57 +0300

    Add file
    Add file java code

---
+++ BasketballPlayer.java
@@ -0,0 +1,8 @@
+package fa.ru.ioc.demo;
+
+public class BasketballPlayer implements SportsPlayer {
+    @Override
+    public void train() {
+        System.out.println("I am trainig on basketball!");
+    }
+}
